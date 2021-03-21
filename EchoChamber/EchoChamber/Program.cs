6ef81c9bb202db565fcc87f3e6661510d6af3a89
LastAeon <53500064+LastAeon@users.noreply.github.com>
2021-03-21 21:26:00 +0700

    error

--- EchoChamber/EchoChamber/Program.cs
+++ EchoChamber/EchoChamber/Program.cs
@@ -14,12 +14,14 @@ namespace EchoChamber
         [STAThread]
         static void Main()
         {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Asus\source\repos\STIMA_Tubes2\EchoChamber\EchoChamber\test.txt");
+            Console.WriteLine(text);
             Graph graph = new Graph();
             List < Vertex > v, v1;
             graph.AddEdge("A", "B");
-            graph.AddEdge("B", "C");
             graph.AddEdge("A", "C");
             graph.AddEdge("C", "D");
+            graph.AddEdge("B", "E");
             //graph.PrintAll();
             graph.FindVertex("A").Print();
             v = graph.GetNDegree(graph.FindVertex("A"), 1);
